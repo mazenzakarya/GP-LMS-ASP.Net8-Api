a9cca4431528f9b46dcfc959b3e490eedcf5883e
ZeyadMashhour <91096543+ZeyadMashhour@users.noreply.github.com>
2025-07-24 13:58:48 +0300

    check it
    mazen check the code

---
+++ GP-LMS-ASP.Net8-Api/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using GP_LMS_ASP.Net8_Api.Context;
+using GP_LMS_ASP.Net8_Api.DTOs;
+using GP_LMS_ASP.Net8_Api.Helpers;
+using GP_LMS_ASP.Net8_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GP_LMS_ASP.Net8_Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        // your code here
+    }
+}
+
---
+++ GP-LMS-ASP.Net8-Api/DTOs/StudentDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class StudentDTO
+{
+	public int UserId { get; set; }
+	public string Name { get; set; }
+	public string? Gender { get; set; }
+	public string? PhoneNumbner { get; set; }
+	public string? Address { get; set; }
+
+}
